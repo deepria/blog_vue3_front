d6df9d6e15b406dc400da1b37fbb5a492643054d
deepria <eojin823@gmail.com>
2025-01-16 16:53:40 +0900

    [업로드] 오탈자 수정

--- src/view/Util/Upload.vue
+++ src/view/Util/Upload.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {onMounted, ref, computed} from "vue";
+import {onMounted, ref} from "vue";
 import {getData, postData} from "@/services/dynamoService.js";
 import {uploadFile, downloadFile, loadFiles, deleteFile} from "@/services/fileService.js";
 
