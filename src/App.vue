200e05fc77a8695c5d306e88e67bd3ad116efe3a
deepria <eojin823@gmail.com>
2025-01-17 10:29:52 +0900

    [Ant] Ant 라이브러리 추가 및 다크테마 적용

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <top-navigation/>
-    <router-view/>
+    <a-config-provider :theme="{ algorithm: theme.darkAlgorithm }">
+      <router-view/>
+    </a-config-provider>
   </div>
 </template>
+
 <script setup>
 import TopNavigation from "@/components/TopNavigation.vue";
+import {theme} from "ant-design-vue";
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,11 +6,20 @@ import {createApp} from 'vue'
 import {createPinia} from "pinia";
 import App from './App.vue'
 import router from "./router"
+import Antd, {message} from 'ant-design-vue';
 
 const app = createApp(App);
 const pinia = createPinia();
 
+message.config({
+    top: "unset",
+    bottom: "50px",
+    duration: 1.5,
+    maxCount: 3,
+});
+
 app.use(router);
 app.use(pinia);
+app.use(Antd)
 app.mount("#app");
 
