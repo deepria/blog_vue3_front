1b235f90fbd4277027cc062d2af2a05461d6f8be
ejlee <eojin823@gmail.com>
2025-06-24 15:56:37 +0900

    [Compressor] 파일명 복호화 예외 처리 추가

--- src/utils/compressor.js
+++ src/utils/compressor.js
@@ -11,9 +11,13 @@ export function compressAndEncode(inputStr) {
 
 // URI 디코딩 + Base64 + 압축 복원
 export function decodeAndDecompress(encodedStr) {
-  const base64 = decodeURIComponent(encodedStr);
-  const binary = atob(base64);
-  const byteArray = new Uint8Array([...binary].map((c) => c.charCodeAt(0)));
-  const decompressed = pako.inflate(byteArray);
-  return new TextDecoder().decode(decompressed);
+  try {
+    const base64 = decodeURIComponent(encodedStr);
+    const binary = atob(base64);
+    const byteArray = new Uint8Array([...binary].map((c) => c.charCodeAt(0)));
+    const decompressed = pako.inflate(byteArray);
+    return new TextDecoder().decode(decompressed);
+  } catch (ignore) {
+    return encodedStr;
+  }
 }
