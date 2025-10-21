0163b4241e942d9fdf3bc07f80f213e687ed0cbc
ejlee <eojin823@gmail.com>
2025-10-21 13:59:49 +0900

    [INDEX] 캔버스 테스트

--- src/view/Index.vue
+++ src/view/Index.vue
@@ -3,12 +3,22 @@
 </template>
 
 <script setup>
-//Created by kusakari
-//https://twitter.com/kusakarism
-
 import { onMounted, onBeforeUnmount } from "vue";
+let canvas;
+/******************
+ Code by Vamoss
+ Original code link:
+ https://www.openprocessing.org/sketch/751983
 
-// 🔧 p5 전역 모드에서 찾을 수 있도록 window에 바인딩
+ Author links:
+ http://vamoss.com.br
+ http://twitter.com/vamoss
+ http://github.com/vamoss
+ ******************/
+
+//Inspired by Felix Auer
+//http://www.felixauer.com/javascript/difeqrk.html
+
 onMounted(() => {
   window.setup = setup;
   window.draw = draw;
@@ -16,369 +26,268 @@ onMounted(() => {
 });
 
 onBeforeUnmount(() => {
-  // p5 스케치 정리(필요 시)
-  // 전역 모드에서는 모든 캔버스를 추적하기 어렵지만,
-  // 다음과 같이 마지막 생성 캔버스가 있다면 제거 가능:
-  if (window._renderer && typeof window._renderer.remove === "function") {
-    window._renderer.remove();
+  // clean up global bindings
+  delete window.setup;
+  delete window.draw;
+  delete window.windowResized;
+
+  // remove canvas if exists
+  if (canvas && typeof canvas.remove === "function") {
+    canvas.remove();
   }
 });
 
-let _minW;
-let _maxW;
-let aryRegionRect = [];
+var blobs = [];
+var colors;
+let variation = 0;
+let xScale, yScale, centerX, centerY;
+//auto change
+let changeDuration = 3000;
+let lastChange = 0;
+// auto emit every 5s
+let autoEmitMs = 5000;
+let lastAutoEmit = 0;
+
+// density/scaling controls for WebView & small screens
+let scaleFactor = 1; // 0.35..1 based on viewport
+let emitCount = 20; // particles per emission
+let fadeAlpha = 10; // trail fade alpha (higher = faster fade)
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  colorMode(HSB, 360, 100, 100, 255);
-  setObject();
+  canvas = createCanvas(windowWidth, windowHeight);
+  // Ensure opaque canvas and reduce WebView compositing issues
+  setAttributes({ alpha: false, antialias: true });
+  // Attach canvas to the Vue container to avoid layout side-effects
+  canvas.parent(document.querySelector(".sketch"));
+  // Optional: stabilize performance on very high-DPI devices (uncomment if needed)
+  pixelDensity(1);
+
+  textAlign(CENTER, CENTER);
+
+  colors = [
+    color("#42b883"), // Vue primary green
+    color("#35495e"), // Vue dark accent
+    color("#3fb984"),
+    color("#2c8e6f"),
+    color("#27ae60"),
+  ];
+  recalcViewportParams();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  setObject();
+  recalcViewportParams();
 }
 
-function setObject() {
-  _minW = min(width, height) * 1;
-  _maxW = max(width, height) * 1;
-  ellipseMode(RADIUS);
-  rectMode(CENTER);
-  strokeWeight(((_minW / 800) * pixelDensity()) / 2);
+function recalcViewportParams() {
+  // base math from original
+  xScale = width / 20;
+  yScale = (height / 20) * (width / height);
+  centerX = width / 2;
+  centerY = height / 2;
 
-  aryRegionRect = [];
+  // scale factor by shorter edge; clamp to avoid too tiny strokes
+  scaleFactor = constrain(min(width, height) / 800, 0.35, 1);
 
-  const numRegionRect = 1;
-  const regionClearanceRatio = 0.3;
-  const minRegionX = (-_minW / 2) * (1 - regionClearanceRatio);
-  const maxRegionX = (_minW / 2) * (1 - regionClearanceRatio);
-  const minRegionY = (-_minW / 2) * (1 - regionClearanceRatio);
-  const maxRegionY = (_minW / 2) * (1 - regionClearanceRatio);
+  // fewer particles on smaller screens
+  emitCount = Math.max(6, Math.round(20 * scaleFactor));
 
-  for (let i = 0; i < numRegionRect; i++) {
-    aryRegionRect.push(
-      new RegionRect(minRegionX, maxRegionX, minRegionY, maxRegionY),
-    );
-  }
+  // stronger fade on smaller screens (to reduce visual clutter)
+  fadeAlpha = Math.round(8 + (1 - scaleFactor) * 12); // ~8..20
 }
 
-class RegionRect {
-  constructor(minRegionX, maxRegionX, minRegionY, maxRegionY) {
-    this.minRegionX = minRegionX;
-    this.maxRegionX = maxRegionX;
-    this.minRegionY = minRegionY;
-    this.maxRegionY = maxRegionY;
-    this.setPolygon();
+function emitParticlesAt(x, y, count = emitCount) {
+  for (let i = 0; i < count; i++) {
+    const rx = x + random(-100, 100);
+    const ry = y + random(-100, 100);
+    const blob = {
+      x: getXPos(rx),
+      y: getYPos(ry),
+      size: random(1, 5) * scaleFactor,
+      lastX: rx,
+      lastY: ry,
+      color: colors[floor(random(colors.length))],
+      direction: random(0.1, 1) * (random() > 0.5 ? 1 : -1),
+    };
+    blobs.push(blob);
   }
-
-  setPolygon() {
-    const maxTrial = 100;
-    const numPolygon = 300;
-    const maxPolygonR =
-      min(
-        this.maxRegionX - this.minRegionX,
-        this.maxRegionY - this.minRegionY,
-      ) / 3;
-    const stepR = _minW / 500;
-    const shrink =
-      min(
-        this.maxRegionX - this.minRegionX,
-        this.maxRegionY - this.minRegionY,
-      ) / 400;
-    const palette = [];
-    this.aryPolygon = [];
-    this.aryAryCornerXy = [];
-
-    for (let i = 0; i < numPolygon; i++) {
-      let areaXy = setAreaXy(
-        this.minRegionX,
-        this.maxRegionX,
-        this.minRegionY,
-        this.maxRegionY,
-      );
-      const rotateAng = random(2 * PI);
-      const numCorner = int(random(3, 9));
-      const numInner = int(random(3, 10));
-      const hi = random(_minW / 200, (_minW / 200) * 10);
-
-      if (i > 0) {
-        let isInside = checkInside(this.aryAryCornerXy, areaXy);
-        let countTrial = 0;
-        while (isInside === true) {
-          countTrial++;
-          areaXy = setAreaXy(
-            this.minRegionX,
-            this.maxRegionX,
-            this.minRegionY,
-            this.maxRegionY,
-          );
-          isInside = checkInside(this.aryAryCornerXy, areaXy);
-          if (countTrial > maxTrial) break;
-        }
-        if (countTrial > maxTrial) break;
-      }
-
-      const [aryCornerXy, areaR] = growPolygon(
-        numCorner,
-        this.aryAryCornerXy,
-        areaXy,
-        rotateAng,
-        maxPolygonR,
-        stepR,
-        this.minRegionX,
-        this.maxRegionX,
-        this.minRegionY,
-        this.maxRegionY,
-      );
-
-      if (areaR > 0) {
-        this.aryPolygon.push(
-          new AreaPolygon(
-            areaXy,
-            areaR,
-            rotateAng,
-            shrink,
-            palette,
-            numInner,
-            numCorner,
-            hi,
-          ),
-        );
-        this.aryAryCornerXy.push(aryCornerXy);
-      }
-    }
-  }
-
-  draw() {
-    push();
-    for (let i = 0; i < this.aryPolygon.length; i++) {
-      this.aryPolygon[i].draw();
-    }
-    pop();
-  }
-}
-
-function setAreaXy(minRegionX, maxRegionX, minRegionY, maxRegionY) {
-  return createVector(
-    random(minRegionX, maxRegionX),
-    random() * (maxRegionY - minRegionY) + minRegionY,
-  );
-}
-
-function growPolygon(
-  numCorner,
-  aryAryXyPolygon,
-  areaXy,
-  rotateAng,
-  maxPolygonR,
-  stepR,
-  minRegionX,
-  maxRegionX,
-  minRegionY,
-  maxRegionY,
-) {
-  let areaR = 0;
-  let isCross = false;
-  const stepAng = (2 * PI) / numCorner;
-
-  while (isCross === false) {
-    areaR += stepR;
-    let aryCornerXy = [];
-    for (let i = 0; i < numCorner; i++) {
-      aryCornerXy[i] = p5.Vector.add(
-        areaXy,
-        createVector(0, -areaR)
-          .rotate(stepAng * (i - 0.5))
-          .rotate(rotateAng),
-      );
-    }
-
-    for (let i = 0; i < aryAryXyPolygon.length; i++) {
-      for (let j = 0; j < aryAryXyPolygon[i].length; j++) {
-        const next_j = (j + 1) % aryAryXyPolygon[i].length;
-        for (let k = 0; k < numCorner; k++) {
-          const next_k = (k + 1) % numCorner;
-          if (
-            checkCrossLineSegment(
-              aryCornerXy[k],
-              aryCornerXy[next_k],
-              aryAryXyPolygon[i][j],
-              aryAryXyPolygon[i][next_j],
-            )
-          ) {
-            isCross = true;
-            break;
-          }
-        }
-        if (isCross) break;
-      }
-      if (isCross) break;
-    }
-
-    for (let i = 0; i < numCorner; i++) {
-      if (
-        aryCornerXy[i].x < minRegionX ||
-        aryCornerXy[i].x > maxRegionX ||
-        aryCornerXy[i].y < minRegionY ||
-        aryCornerXy[i].y > maxRegionY
-      ) {
-        isCross = true;
-      }
-    }
-
-    if (isCross === true) areaR -= stepR;
-    if (areaR > maxPolygonR) {
-      areaR = maxPolygonR;
-      break;
-    }
-  }
-
-  const aryCornerXy = [];
-  for (let i = 0; i < numCorner; i++) {
-    aryCornerXy[i] = p5.Vector.add(
-      areaXy,
-      createVector(0, -areaR)
-        .rotate(stepAng * (i - 0.5))
-        .rotate(rotateAng),
-    );
-  }
-
-  return [aryCornerXy, areaR];
-}
-
-function checkCrossLineSegment(xy_a, xy_b, xy_c, xy_d) {
-  const vec_a_b = p5.Vector.sub(xy_b, xy_a);
-  const vec_a_c = p5.Vector.sub(xy_c, xy_a);
-  const vec_a_d = p5.Vector.sub(xy_d, xy_a);
-  const vec_c_d = p5.Vector.sub(xy_d, xy_c);
-  const vec_c_a = p5.Vector.sub(xy_a, xy_c);
-  const vec_c_b = p5.Vector.sub(xy_b, xy_c);
-  const cr_ab_ac = p5.Vector.cross(vec_a_b, vec_a_c);
-  const cr_ab_ad = p5.Vector.cross(vec_a_b, vec_a_d);
-  const cr_cd_ca = p5.Vector.cross(vec_c_d, vec_c_a);
-  const cr_cd_cb = p5.Vector.cross(vec_c_d, vec_c_b);
-  return cr_ab_ac.z * cr_ab_ad.z <= 0 && cr_cd_ca.z * cr_cd_cb.z <= 0;
-}
-
-function checkInside(aryAryXy, areaXy) {
-  let isInside = false;
-  for (let i = 0; i < aryAryXy.length; i++) {
-    for (let j = 0; j < aryAryXy[i].length; j++) {
-      const next_j = (j + 1) % aryAryXy[i].length;
-      const vec_a_b = p5.Vector.sub(aryAryXy[i][next_j], aryAryXy[i][j]);
-      const vec_a_c = p5.Vector.sub(areaXy, aryAryXy[i][j]);
-      const cr_ab_ac = p5.Vector.cross(vec_a_b, vec_a_c);
-      if (cr_ab_ac.z < 0) break;
-      else if (j === aryAryXy[i].length - 1) isInside = true;
-    }
-    if (isInside) break;
-  }
-  return isInside;
-}
-
-class AreaPolygon {
-  constructor(
-    areaXy,
-    areaR,
-    rotateAng,
-    shrink,
-    palette,
-    numInner,
-    numCorner,
-    hi,
-  ) {
-    this.areaXy = areaXy;
-    this.areaR = areaR;
-    this.rotateAng = rotateAng;
-    this.shrink = shrink;
-    this.r = this.areaR - this.shrink;
-    this.numCorner = numCorner;
-    this.stepAng = (2 * PI) / this.numCorner;
-    this.hi = hi;
-    this.hiStep = this.hi / 2;
-
-    this.palette = palette;
-    this.numInner = numInner;
-    this.setInner();
-  }
-
-  setInner() {
-    this.aryInnerR = [];
-    this.aryInnerAng = [];
-    this.aryGrad = [];
-    const stepR = (this.r / ((this.numInner + 1) * 2 - 1)) * 2;
-    for (let i = 0; i < this.numInner; i++) {
-      this.aryInnerR[i] = this.r - stepR * (i + 1);
-      this.aryInnerAng[i] =
-        ((2 * PI) / this.numCorner) * int(random(this.numCorner));
-    }
-  }
-
-  draw() {
-    if (this.r > 0) {
-      push();
-      fill(0, 0, 100);
-      stroke(0, 0, 0);
-      translate(this.areaXy.x, this.areaXy.y);
-      drawCylinder(this.numCorner, this.r, this.hi, this.rotateAng, 16);
-      pop();
-      this.drawInner();
-    }
-  }
-
-  drawInner() {
-    for (let i = 0; i < this.numInner; i++) {
-      push();
-      fill(0, 0, 100);
-      stroke(0, 0, 0);
-      translate(this.areaXy.x, this.areaXy.y, this.hi + this.hiStep * i);
-      drawCylinder(
-        this.numCorner,
-        this.aryInnerR[i],
-        this.hiStep,
-        this.rotateAng,
-        Math.round(1 + (15 * (1 - (1 / this.numInner) * (i + 1))) / 2),
-      );
-      pop();
-    }
-  }
-}
-
-function drawCylinder(numCorner, r, hi, rotateAng, detailY) {
-  push();
-  rotateX(PI / 2);
-  if (numCorner % 2 === 0) rotateY((-2 * PI) / numCorner / 2);
-  translate(0, hi / 2);
-  rotateY(rotateAng + PI);
-  cylinder(r, hi, numCorner + 1, detailY, true, true);
-  pop();
 }
 
 function draw() {
-  orbitControl();
-  background(0, 0, 0);
-  ortho(-width / 2, width / 2, -height / 2, height / 2, 0, width * 2);
-  translate(0, _minW / 10, 0);
-  rotateX(PI / 2 - PI / 6);
-  rotateZ(PI / 4);
-  rotateZ((frameCount - 1) * 0.003);
+  //DEBUG
+  // textSize(20);
+  // noStroke();
+  // fill(255);
+  // ellipse(centerX, centerY, 60, 60);
+  // fill(0);
+  // text(variation, centerX, centerY - 10);
+  // text(length, centerX, centerY + 10);
 
-  for (let i = 0; i < aryRegionRect.length; i++) {
-    aryRegionRect[i].draw();
+  // auto emit at random position every 5s (even without user touch)
+  const now = millis();
+  if (now - lastAutoEmit > autoEmitMs) {
+    lastAutoEmit = now;
+    emitParticlesAt(random(width), random(height), emitCount);
   }
+
+  if (mouseIsPressed) {
+    for (let i = 0; i < emitCount; i++) {
+      let x = mouseX + random(-100, 100);
+      let y = mouseY + random(-100, 100);
+      var blob = {
+        x: getXPos(x),
+        y: getYPos(y),
+        size: random(1, 5) * scaleFactor,
+        lastX: x,
+        lastY: y,
+        color: colors[floor(random(colors.length))],
+        direction: random(0.1, 1) * (random() > 0.5 ? 1 : -1),
+      };
+      blobs.push(blob);
+    }
+  }
+
+  var length = blobs.length;
+  if (length == 0) {
+    // pure black background when no particles yet
+    background(0);
+  } else {
+    // fade trail on black
+    noStroke();
+    fill(0, 0, 0, fadeAlpha);
+    rect(0, 0, width, height);
+  }
+
+  //auto change
+  let time = millis();
+  if (time - lastChange > changeDuration) {
+    lastChange = time;
+    variation++;
+    if (variation > 11) variation = 0;
+  }
+
+  var stepsize = deltaTime * 0.002;
+  for (var i = length - 1; i >= 0; i--) {
+    let blob = blobs[i];
+
+    var x = getSlopeX(blob.x, blob.y);
+    var y = getSlopeY(blob.x, blob.y);
+    blob.x += blob.direction * x * stepsize;
+    blob.y += blob.direction * y * stepsize;
+
+    x = getXPrint(blob.x);
+    y = getYPrint(blob.y);
+    stroke(blob.color);
+    strokeWeight(blob.size);
+    line(x, y, blob.lastX, blob.lastY);
+    blob.lastX = x;
+    blob.lastY = y;
+
+    const border = 200;
+    if (
+      x < -border ||
+      y < -border ||
+      x > width + border ||
+      y > height + border
+    ) {
+      blobs.splice(i, 1);
+    }
+  }
+}
+
+function getSlopeY(x, y) {
+  switch (variation) {
+    case 0:
+      return Math.sin(x);
+    case 1:
+      return Math.sin(x * 5) * y * 0.3;
+    case 2:
+      return Math.cos(x * y);
+    case 3:
+      return Math.sin(x) * Math.cos(y);
+    case 4:
+      return Math.cos(x) * y * y;
+    case 5:
+      return Math.log(Math.abs(x)) * Math.log(Math.abs(y));
+    case 6:
+      return Math.tan(x) * Math.cos(y);
+    case 7:
+      return -Math.sin(x * 0.1) * 3; //orbit
+    case 8:
+      return (x - x * x * x) * 0.01; //two orbits
+    case 9:
+      return -Math.sin(x);
+    case 10:
+      return -y - Math.sin(1.5 * x) + 0.7;
+    case 11:
+      return Math.sin(x) * Math.cos(y);
+  }
+}
+
+function getSlopeX(x, y) {
+  switch (variation) {
+    case 0:
+      return Math.cos(y);
+    case 1:
+      return Math.cos(y * 5) * x * 0.3;
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      return 1;
+    case 7:
+      return Math.sin(y * 0.1) * 3; //orbit
+    case 8:
+      return y / 3; //two orbits
+    case 9:
+      return -y;
+    case 10:
+      return -1.5 * y;
+    case 11:
+      return Math.sin(y) * Math.cos(x);
+  }
+}
+
+function getXPos(x) {
+  return (x - centerX) / xScale;
+}
+function getYPos(y) {
+  return (y - centerY) / yScale;
+}
+
+function getXPrint(x) {
+  return xScale * x + centerX;
+}
+function getYPrint(y) {
+  return yScale * y + centerY;
 }
 </script>
 
 <style scoped>
+:root,
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+  overflow: hidden; /* prevent scroll */
+  overscroll-behavior: none; /* block iOS/Android WebView bounce */
+  touch-action: none; /* avoid touch scroll while interacting */
+}
+
 .sketch {
   width: 100%;
   height: 100%;
   position: fixed;
   inset: 0;
+  z-index: 1; /* keep canvas above backgrounds */
+  background: transparent; /* canvas is opaque due to alpha:false */
 }
-html,
-body,
-#app {
-  height: 100%;
+
+.sketch canvas {
+  display: block; /* remove inline gap causing scroll */
+  width: 100% !important;
+  height: 100% !important;
 }
 </style>
